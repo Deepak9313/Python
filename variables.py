61031e9a38a9e271a24e24696aa8269a66c18e99
Deepak <deepak9313@gmail.com>
2024-04-12 17:27:47 +0530

    First Day

---
+++ variables.py
@@ -0,0 +1,29 @@
+a = 23
+b = "python"
+c = 12.55
+print(type(a))
+print(type(b))
+print(type(c))
+print("The data of three variables is {},{} and {}".format(a,b,c)) # homework
+# Single line comment
+'''
+Multline 
+comment
+'''
+# rule 1: variable name can't start with a number : example 123abc 99student
+# rule 2 : variable can take a numeric value in it : example abc12
+# rule 3 : variable can start with a underscore : example _abc 
+# rule 4 : you can't make a variable with a identifer or a keyword := like print
+# rule 5 : we can create variables in upper case and lower case :- age AGE
+age = 10
+Age = 20
+AGE = 30
+print(age)
+print(Age)
+print(AGE)
+# constant means you can't change its value after intialization
+pi = 3.14
+pi = 45
+print(pi)
+PI = 3.14 # you can write the constant in upper case
+print(PI)
